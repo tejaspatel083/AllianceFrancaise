beb5d5cdc92fe845407f203f564e504821484afa
TejasPatel <tjp083@gmail.com>
2021-02-27 19:48:35 -0500

    Profile UI is done

---
+++ app/src/main/java/com/example/project_af/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.example.project_af;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_profile,container,false);
+        getActivity().setTitle("Profile");
+
+
+        return view;
+    }
+}
